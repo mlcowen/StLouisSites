a3b9804d44337a9fea1d812826f0e48650ac75c9
Marie Cowen <marie.cowen.87@gmail.com>
2019-06-05 20:07:22 -0500

    In class work

---
+++ StLouisSites/Controllers/LocationController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StLouisSites.Data;
+using StLouisSites.Models;
+
+namespace StLouisSites.Controllers
+{
+    public class LocationController : Controller
+    {
+        private ApplicationDbContext context;
+
+        public LocationController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+           
+
+        public IActionResult Index()
+        {
+            List<Location> locations = context.Locations.ToList();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Location location)
+        {
+            context.Add(location);
+            context.SaveChanges();
+            //If you add and don't save changes, then it won't save to the database. 
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
