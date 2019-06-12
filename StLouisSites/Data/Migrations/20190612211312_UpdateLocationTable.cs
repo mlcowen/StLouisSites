c686abfd1263e8ebd00ca0757580f4ef0c4ee8c8
Marie Cowen <marie.cowen.87@gmail.com>
2019-06-12 16:21:16 -0500

    Added new migration

---
+++ StLouisSites/Data/Migrations/20190612211312_UpdateLocationTable.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StLouisSites.Data.Migrations
+{
+    public partial class UpdateLocationTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Locations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "HoursOfOperation",
+                table: "Locations",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Locations");
+
+            migrationBuilder.DropColumn(
+                name: "HoursOfOperation",
+                table: "Locations");
+        }
+    }
+}
