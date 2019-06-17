2cd965a367bc526c41829f28a5835e5ba6290c80
Marie Cowen <marie.cowen.87@gmail.com>
2019-06-17 15:21:43 -0500

    partially completed stories

---
+++ StLouisSites/Controllers/LocationRatingController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StLouisSites.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StLouisSites.Controllers
+{
+    public class LocationRatingController : Controller
+    {
+        private ApplicationDbContext context;
+
+        public LocationRatingController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Create(int locationId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(int locationId, LocationRatingCreateViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            model.Persist(context);
+            return RedirectToAction(controllerName: "Location", actionName: "Index");
+        }
+    }
+}
---
+++ StLouisSites/Models/LocationRating.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StLouisSites.Models
+{
+    public class LocationRating
+    {
+        public int Id { get; set; }
+        public int Rating { get; set; }
+        public int LocationId { get; set; }
+        public string Review { get; set; }
+    }
+}
