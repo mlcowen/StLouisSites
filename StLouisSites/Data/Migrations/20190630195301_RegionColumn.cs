910e1363b57ab038e4b34c443c5e2aafa8bc8e21
Marie Cowen <marie.cowen.87@gmail.com>
2019-06-30 14:54:17 -0500

    adding region column to migration

---
+++ StLouisSites/Data/Migrations/20190630195301_RegionColumn.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StLouisSites.Data.Migrations
+{
+    public partial class RegionColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Region",
+                table: "Locations",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Region",
+                table: "Locations");
+        }
+    }
+}
